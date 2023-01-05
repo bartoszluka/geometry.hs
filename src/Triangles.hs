3f31071e84feddbe0b8b6dff3896fe7109aa9e98
Bartek <bartek@Bartek-komputer.localdomain>
2023-01-05 16:01:20 +0100

    Reimplement functionality using a differenet data type

---
+++ src/Triangles.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
+module Triangles (
+    lineThrough,
+    intersection,
+    doubleEq,
+    Line (..),
+    Point,
+    baroCenter,
+    perpendicularThrough,
+    toStr,
+) where
+
+import Text.Printf (printf)
+
+type Point = (Double, Double)
+
+data Line = Vertical Double | Line {coefA :: Double, coefB :: Double}
+
+epsilon :: Double
+epsilon = 1e-5
+
+toStr :: Line -> String
+toStr (Vertical x) =
+    printf "x = %.2f" x
+-- y = 2.00x + 5.00
+-- y = -2.00x - 0.69
+toStr (Line{coefA, coefB}) =
+    printf "y = %.2fx%s%.2f" coefA (if coefB < 0 then " - " else " + ") (abs coefB)
+
+instance Show Line where
+    show = toStr
+
+instance Eq Line where
+    (Vertical x1) == (Vertical x2) = doubleEq x1 x2
+    (Line{coefA = a1, coefB = b1}) == (Line{coefA = a2, coefB = b2}) = doubleEq a1 a2 && doubleEq b1 b2
+    _ == _ = False
+
+doubleEq :: Double -> Double -> Bool
+doubleEq x y = abs (x - y) < epsilon
+
+lineThrough :: Point -> Point -> Line
+lineThrough (xa, ya) (xb, yb) =
+    case xa - xb of
+        0 -> Vertical xa
+        _nonZero ->
+            Line
+                { coefA = (ya - yb) / (xa - xb)
+                , coefB = (ya - yb) / (xa - xb) * xb - yb
+                }
+
+baroCenter :: Point -> Point -> Point -> Point
+baroCenter pointA pointB pointC =
+    intersection height1 height2
+  where
+    height1 = perpendicularThrough pointC (lineThrough pointA pointB)
+    height2 = perpendicularThrough pointB (lineThrough pointA pointC)
+
+intersection :: Line -> Line -> Point
+intersection (Line{coefA = a1, coefB = b1}) (Line{coefA = a2, coefB = b2}) =
+    let a = -(b1 - b2) / (a1 - a2)
+     in (a, a * a1 + b1)
+intersection (Line{coefA = a, coefB = b}) (Vertical x) =
+    (x, a * x + b)
+intersection (Vertical x) (Line{coefA = a, coefB = b}) =
+    (x, a * x + b)
+intersection (Vertical _x) (Vertical _x') = let infinity = 1 / 0 in (infinity, infinity)
+
+perpendicularThrough :: Point -> Line -> Line
+perpendicularThrough (_px, py) (Vertical _) = Line{coefA = 0, coefB = py}
+perpendicularThrough (px, py) (Line{coefA = a, coefB = _}) =
+    if a == 0
+        then Vertical px
+        else Line{coefA = -1 / a, coefB = py + 1 / a * px}
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,51 @@
-import Lib (Point, intersection)
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use <$>" #-}
+
+import Test.Hspec (describe, hspec, it, shouldBe, shouldSatisfy)
+import Test.QuickCheck
+import Triangles (Line (..), Point, baroCenter, doubleEq, intersection, lineThrough, perpendicularThrough)
+
+instance Arbitrary Line where
+    arbitrary = do
+        let randDouble :: Gen Double
+            randDouble = suchThat arbitrary (\x -> not (isInfinite x || isNaN x))
+        n <- chooseInt (1, 10)
+        if n == 1
+            then Vertical <$> randDouble
+            else do
+                a <- randDouble
+                b <- randDouble
+                return $ Line a b
 
 main :: IO ()
 main = hspec $ do
-    describe "intersection" $ do
+    describe "lineThrough" $ do
         it "should give the same line if the same points are given in opposite order" $ do
-            property $ \(p1, p2 :: (Point, Point)) -> intersection p1 p2 `shouldBe` intersection p2 p1
+            property $ \((p1, p2) :: (Point, Point)) -> lineThrough p1 p2 `shouldBe` lineThrough p2 p1
+
+    describe "intersection" $ do
+        it "should give the same point if the same lines are given in opposite order" $ do
+            property $ \((line1, line2) :: (Line, Line)) ->
+                (line1 /= line2)
+                    ==> let (x1, y1) = intersection line1 line2
+                            (x2, y2) = intersection line2 line1
+                         in do
+                                x1 `shouldSatisfy` doubleEq x2
+                                y1 `shouldSatisfy` doubleEq y2
+
+    describe "perpendicularThrough" $ do
+        it "gives the line that's perpendicular to a given line and goes throug a given point" $ do
+            perpendicularThrough (2, 1) (Line 0.5 0) `shouldBe` Line (-2) 5
+            perpendicularThrough (2, 1) (Vertical 5) `shouldBe` Line 0 1
+        it "applies perpendicularThrough twice to get the same line" $ do
+            property $ \((startingLine, point) :: (Line, Point)) ->
+                let secondLine = perpendicularThrough point startingLine
+                    intersectionPoint = intersection secondLine startingLine
+                 in perpendicularThrough intersectionPoint secondLine `shouldBe` startingLine
+
+    describe "baroCenter" $ do
+        it "gives barometric center of a triangle" $ do
+            baroCenter (0, 0) (3, 0) (0, 4) `shouldBe` (0, 0)
